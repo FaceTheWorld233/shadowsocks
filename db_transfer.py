0d7e71a1aa4f6ac2871ed228a31fb1df5c53030d
FaceTheWorld <ssr@tgssr.cn>
2020-01-04 01:09:11 +0800

    Update db_transfer.py

--- db_transfer.py
+++ db_transfer.py
@@ -338,8 +338,12 @@ class DbTransfer(object):
         conn.autocommit(True)
 
         cur = conn.cursor()
-        cur.execute("SELECT `node_group`,`node_class`,`node_speedlimit`,`traffic_rate`,`mu_only`,`sort`,`port_offset`,`set_back_port` FROM ss_node where `id`='" +
-                    str(get_config().NODE_ID) + "' AND (`node_bandwidth`<`node_bandwidth_limit` OR `node_bandwidth_limit`=0)")
+        if get_config().MYSQL_OFFSET == 0:
+            cur.execute("SELECT `node_group`,`node_class`,`node_speedlimit`,`traffic_rate`,`mu_only`,`sort`,`port_offset` FROM ss_node where `id`='" +
+                        str(get_config().NODE_ID) + "' AND (`node_bandwidth`<`node_bandwidth_limit` OR `node_bandwidth_limit`=0)")
+        elif get_config().MYSQL_OFFSET != 0:
+            cur.execute("SELECT `node_group`,`node_class`,`node_speedlimit`,`traffic_rate`,`mu_only`,`sort` FROM ss_node where `id`='" +
+                        str(get_config().NODE_ID) + "' AND (`node_bandwidth`<`node_bandwidth_limit` OR `node_bandwidth_limit`=0)")
         nodeinfo = cur.fetchone()
 
         if nodeinfo is None:
@@ -375,15 +379,13 @@ class DbTransfer(object):
                     node_group_sql +
                     ") OR `is_admin`=1) AND`enable`=1 AND `expire_in`>now() AND `transfer_enable`>`u`+`d`")
         rows = []
-        offset = int(nodeinfo[6])
-        set_back_port = int(nodeinfo[7])
+        if get_config().MYSQL_OFFSET == 0:
+            offset = int(nodeinfo[6])
         for r in cur.fetchall():
             d = {}
             for column in range(len(keys)):
                 d[keys[column]] = r[column]
-                if set_back_port != 0:
-                    d['port'] = set_back_port
-                else:
+                if get_config().MYSQL_OFFSET == 0:
                     d['port'] = r[1] + offset
             rows.append(d)
         cur.close()
@@ -523,7 +525,11 @@ class DbTransfer(object):
 
         for row in rows:            
             if row['is_multi_user'] != 0:
-                self.mu_port_list.append(int(row['port']))
+                if get_config().MYSQL_OFFSET == 0:
+                    self.mu_port_list.append(int(row['port']))
+                elif get_config().MYSQL_OFFSET != 0:
+                    #端口偏移
+                    row['port'] = row['port']+get_config().MYSQL_OFFSET
                 continue
 
             md5_users[row['id']] = row.copy()
